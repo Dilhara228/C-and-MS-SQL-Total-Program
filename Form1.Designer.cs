3915c94e60dea0536cf1917b7c52b19c75af0b1b
ThiliniDilhara <thilinidilhara@novatechzone.com>
2024-08-01 14:23:50 +0530

    total program commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace TotalProgram
+{
+    partial class frmFindTotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNo1 = new System.Windows.Forms.Label();
+            this.lblNo2 = new System.Windows.Forms.Label();
+            this.lblNo3 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtNo1 = new System.Windows.Forms.TextBox();
+            this.txtNo2 = new System.Windows.Forms.TextBox();
+            this.txtNo3 = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblLargeValue = new System.Windows.Forms.Label();
+            this.txtLargeValue = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNo1
+            // 
+            this.lblNo1.AutoSize = true;
+            this.lblNo1.Location = new System.Drawing.Point(52, 21);
+            this.lblNo1.Name = "lblNo1";
+            this.lblNo1.Size = new System.Drawing.Size(67, 13);
+            this.lblNo1.TabIndex = 0;
+            this.lblNo1.Text = "Number One";
+            // 
+            // lblNo2
+            // 
+            this.lblNo2.AutoSize = true;
+            this.lblNo2.Location = new System.Drawing.Point(52, 69);
+            this.lblNo2.Name = "lblNo2";
+            this.lblNo2.Size = new System.Drawing.Size(68, 13);
+            this.lblNo2.TabIndex = 1;
+            this.lblNo2.Text = "Number Two";
+            // 
+            // lblNo3
+            // 
+            this.lblNo3.AutoSize = true;
+            this.lblNo3.Location = new System.Drawing.Point(52, 118);
+            this.lblNo3.Name = "lblNo3";
+            this.lblNo3.Size = new System.Drawing.Size(75, 13);
+            this.lblNo3.TabIndex = 2;
+            this.lblNo3.Text = "Number Three";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(52, 171);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(31, 13);
+            this.lblTotal.TabIndex = 3;
+            this.lblTotal.Text = "Total";
+            // 
+            // txtNo1
+            // 
+            this.txtNo1.Location = new System.Drawing.Point(416, 21);
+            this.txtNo1.Name = "txtNo1";
+            this.txtNo1.Size = new System.Drawing.Size(100, 20);
+            this.txtNo1.TabIndex = 4;
+            // 
+            // txtNo2
+            // 
+            this.txtNo2.Location = new System.Drawing.Point(416, 69);
+            this.txtNo2.Name = "txtNo2";
+            this.txtNo2.Size = new System.Drawing.Size(100, 20);
+            this.txtNo2.TabIndex = 5;
+            // 
+            // txtNo3
+            // 
+            this.txtNo3.Location = new System.Drawing.Point(416, 118);
+            this.txtNo3.Name = "txtNo3";
+            this.txtNo3.Size = new System.Drawing.Size(100, 20);
+            this.txtNo3.TabIndex = 6;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(416, 171);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 20);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(55, 387);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 8;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(451, 387);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear.Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(52, 227);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(47, 13);
+            this.lblAverage.TabIndex = 10;
+            this.lblAverage.Text = "Average";
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(416, 227);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.Size = new System.Drawing.Size(100, 20);
+            this.txtAverage.TabIndex = 11;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(52, 280);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(36, 13);
+            this.lblResult.TabIndex = 12;
+            this.lblResult.Text = "Grade";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(416, 280);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 13;
+            // 
+            // lblLargeValue
+            // 
+            this.lblLargeValue.AutoSize = true;
+            this.lblLargeValue.Location = new System.Drawing.Point(53, 333);
+            this.lblLargeValue.Name = "lblLargeValue";
+            this.lblLargeValue.Size = new System.Drawing.Size(64, 13);
+            this.lblLargeValue.TabIndex = 14;
+            this.lblLargeValue.Text = "Large Value";
+            // 
+            // txtLargeValue
+            // 
+            this.txtLargeValue.Location = new System.Drawing.Point(416, 333);
+            this.txtLargeValue.Name = "txtLargeValue";
+            this.txtLargeValue.Size = new System.Drawing.Size(100, 20);
+            this.txtLargeValue.TabIndex = 15;
+            // 
+            // frmFindTotal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 422);
+            this.Controls.Add(this.txtLargeValue);
+            this.Controls.Add(this.lblLargeValue);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtNo3);
+            this.Controls.Add(this.txtNo2);
+            this.Controls.Add(this.txtNo1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblNo3);
+            this.Controls.Add(this.lblNo2);
+            this.Controls.Add(this.lblNo1);
+            this.Name = "frmFindTotal";
+            this.Text = "Find Total";
+            this.Load += new System.EventHandler(this.frmFindTotal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNo1;
+        private System.Windows.Forms.Label lblNo2;
+        private System.Windows.Forms.Label lblNo3;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtNo1;
+        private System.Windows.Forms.TextBox txtNo2;
+        private System.Windows.Forms.TextBox txtNo3;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblLargeValue;
+        private System.Windows.Forms.TextBox txtLargeValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotalProgram
+{
+    public partial class frmFindTotal : Form
+    {
+        public frmFindTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFindTotal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            txtTotal.Text = Convert.ToString(Convert.ToDouble(txtNo1.Text) + Convert.ToDouble(txtNo2.Text) + Convert.ToDouble(txtNo3.Text));
+            txtAverage.Text = Convert.ToString(Convert.ToDouble(txtTotal.Text) / 3);
+            if (Convert.ToDouble(txtAverage.Text) >= 75)
+            {
+                txtResult.Text = "Very Good";
+            }
+            else
+            {
+                if (Convert.ToDouble(txtAverage.Text) >= 65)
+                {
+                    txtResult.Text = "Good";
+                }
+                else
+                {
+                    if (Convert.ToDouble(txtAverage.Text) >= 55)
+                    {
+                        txtResult.Text = "Pass";
+                    }
+                    else
+                    {
+                        txtResult.Text = "Fail";
+                    }
+                }
+            }
+            double No1 = Convert.ToDouble(txtNo1.Text);
+            double No2 = Convert.ToDouble(txtNo2.Text);
+            double No3 = Convert.ToDouble(txtNo3.Text);
+            if (No1 > No2)
+            {
+                if (No1 > No3)
+                {
+                    //txtLargestValue.Text = Convert.ToString(No1);
+                    txtLargeValue.Text = txtNo1.Text;
+                }
+                else
+                {
+                    txtLargeValue.Text = txtNo3.Text;
+                }
+            }
+            else
+            {
+                if (No2 > No3)
+                {
+                    txtLargeValue.Text = txtNo2.Text;
+                }
+                else
+                {
+                    txtLargeValue.Text = txtNo3.Text;
+                }
+
+            }
+         
+
+            {
+
+            }
+
+        }
+    }
+}
